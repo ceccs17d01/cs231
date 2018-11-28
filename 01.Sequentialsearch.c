6a6da6c9aab654972f5514aeba53b8215593db80
ceccs17d01 <43025232+ceccs17d01@users.noreply.github.com>
2018-11-28 18:58:34 +0530

    Add files via upload

---
+++ 01.Sequentialsearch.c
@@ -0,0 +1,54 @@
+/*ABIN THOMAS
+	S3 D 01
+	SEQUENTIAL SEARCH */
+
+
+#include <stdio.h>
+
+int sequentialSearch(int array[], int size, int element) {
+	int i;
+
+	for (i=0; i<size; i++) {
+		if (array[i] == element) {
+			return (i+1);
+		}
+	}
+	return (0);
+}
+int main() {
+	int i=0, found=0, array[100], size, element;
+
+	printf("Enter number of elements : ");
+	scanf("%d", &size);
+
+	printf("Enter elements\n");
+	for (i=0; i<size; i++) {
+		scanf("%d", &array[i]);
+	}
+
+	printf("Enter element to search : ");
+	scanf("%d", &element);
+
+	found = sequentialSearch(array, size, element);
+
+	if (found) {
+		printf("Element found at position %d\n", found);
+	} else {
+		printf("Element not found\n");
+	}
+}
+
+
+
+
+	OUTPUT:
+	Enter number of elements : 5
+Enter elements
+12
+123
+23
+42
+2
+Enter element to search : 2
+Element found at position 5
+
