b00cf6050c6e027658cb1e5b558bd2b17088d8d3
ceccs17d01 <43025232+ceccs17d01@users.noreply.github.com>
2018-11-29 15:01:36 +0530

    Add files via upload

---
+++ 21.arithametic_expression_usingLL.c
@@ -0,0 +1,77 @@
+/*ABIN THOMAS
+S3 D 01
+  Arithametic Expression using LinkedList*/
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+	char data;
+	struct node *next;
+}*HEADER;
+int deleteAll() {
+	struct node *ptr = HEADER, *prevPtr;
+	while (ptr != NULL) {
+		prevPtr = ptr;
+		ptr = ptr->next;
+		free(prevPtr);
+	}
+	HEADER->next = NULL;
+	return (0);
+}
+struct node *newNode() {
+	struct node *newptr = malloc(sizeof(struct node));
+	if (newptr == NULL) {
+		printf("Memory overflow");
+		deleteAll();
+		exit(0);
+	}
+	return (newptr);
+}
+int insertNodeEnd(int data) {
+	struct node *newptr = newNode(), *currentNode = HEADER;
+
+	while (currentNode->next != NULL) {
+		currentNode = currentNode->next;
+	}
+
+	newptr->next = currentNode->next;
+	newptr->data = data;
+	currentNode->next = newptr;
+	return (0);
+}
+
+int printLinkedList() {
+	struct node *currentNode = HEADER->next;
+	while (currentNode != NULL) {
+		printf("%c", currentNode->data);
+		currentNode = currentNode->next;
+	}
+  printf("\n");
+	return (0);
+}
+
+int main() {
+  char infix[100], buffer;
+  int i=0;
+	// initialize Linked List
+	HEADER = malloc(sizeof(struct node));
+	HEADER->data = 0;
+	HEADER->next = NULL;
+	// finished initializing Linked List
+
+  printf("Enter arithmatic expression : ");
+  scanf("%s", infix);
+  for (i=0; infix[i]!='\0'; i++) {
+    insertNodeEnd(infix[i]);
+  }
+  printf("Entered experssion : ");
+  printLinkedList();
+
+	deleteAll();
+	
+	return (0);
+}
+OUTPUT:
+Enter arithmatic expression : 54+5-12*2
+Entered experssion : 54+5-12*2
+
